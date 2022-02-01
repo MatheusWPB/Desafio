81de554ce76454ed5eb766fab9c35f2901ea85ab
MatheusWPB <98498520+MatheusWPB@users.noreply.github.com>
2022-02-01 11:50:41 -0300

    remove

--- desafio 1.py
+++
@@ -1,7 +0,0 @@
-a = float (input('digite um valor para as operações: '))
-
-m = 2*a
-m2 = 3*a
-r = a**1/2
-
-print ('O dobro do valor é {} o tripo {} e a raiz quadrada vale {}'.format(m,m2,r))
--- desafio 2.py
+++
@@ -1,6 +0,0 @@
-a = float(input('digite aprimeira nota do aluno')) 
-
-a2=float(input('digite a segunda notado aluno'))
-m = (a+ a2)/2
-
-print (m)
